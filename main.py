2fe07a5ccf8e340df1dee964cec80f0b0f5b9b2f
Francesco Miliani <france.mi18@gmail.com>
2024-02-08 08:40:15 +0100

    Add completion example

---
+++ main.py
@@ -0,0 +1,56 @@
+## Description: This is a simple example of how to use the AI21 API to generate text using the J2-Mid model.
+## Author: Francesco Miliani
+## Date: 2024-02-08
+## Reference: https://docs.ai21.com/reference/python-sdk
+
+
+from dotenv import load_dotenv
+import os, json
+from ai21 import AI21Client
+# Load variables from the .env file
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
+print(f"\t API_KEY: {API_KEY}")
+ 
+client = AI21Client(api_key=API_KEY)
+
+_prompt = """
+Complete the following sentence.
+Sentence: These are a few of my favorite
+Completion: things. Cream-colored ponies and crisp apple strudels.
+
+##
+
+Complete the following sentence.
+Sentence: These are a few of my favorite
+Completion: rock bands: The Beatles, The Doors, Fleetwood Mac.
+
+##
+
+Complete the following sentence.
+Sentence: These are a few of my favorite
+Completion:
+"""
+
+# J2 Mid
+
+print(f'Prompt: {_prompt}')
+
+## Example 1 - Completion
+response_mid = client.completion.create(
+  model="j2-mid",
+  #prompt="These are a few of my favorite",
+  prompt=_prompt,
+  num_results=1,
+  max_tokens=2,
+  temperature=0.4,
+  top_k_return=0,
+  top_p=1,
+  stop_sequences=["##"]
+)
+
+# print(response_mid)
+for i in range(len(response_mid.completions)):
+    print(response_mid.completions[i].data.text)
+    print("\n")
